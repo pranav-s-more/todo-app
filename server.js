bef1fef504c9a4c6a923faf729ca37fb5140bbd1
Pranav More <pranavmore269@gmail.com>
2026-07-01 16:08:19 +0530

    added env file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
